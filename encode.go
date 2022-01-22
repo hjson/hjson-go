459d0efbe6cdd5c2ab0e3dc41881e8ca3baf18f3
bronze1man <bronze1man@gmail.com>
2022-01-22 11:00:01 +0800

    fix bug of encode []**string

--- encode.go
+++ encode.go
@@ -207,8 +207,7 @@ func (e *hjsonEncoder) str(value reflect.Value, noIndent bool, separator string,
 			e.WriteString("null")
 			return nil
 		}
-		value = value.Elem()
-		kind = value.Kind()
+		return e.str(value.Elem(),noIndent,separator,isRootObject)
 	}
 
 	if value.Type().Implements(marshaler) {

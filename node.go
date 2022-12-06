33e01a6421d132741d0cc5e7733300a3191abc7a
trobro <git@trobro.se>
2022-12-06 10:20:50 +0100

    node set get

--- node.go
+++ node.go
@@ -2,6 +2,8 @@ package hjson
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 )
 
 type Comments struct {
@@ -14,6 +16,97 @@ type Comments struct {
 type Node struct {
 	Value interface{}
 	Cm    Comments
+}
+
+func (c Node) Len() int {
+	switch cont := c.Value.(type) {
+	case *OrderedMap:
+		return cont.Len()
+	case []interface{}:
+		return len(cont)
+	case string:
+		return len(cont)
+	}
+	return 0
+}
+
+func (c Node) GetIndex(index int) (interface{}, error) {
+	var elem interface{}
+
+	switch cont := c.Value.(type) {
+	case *OrderedMap:
+		elem = cont.AtIndex(index)
+	case []interface{}:
+		elem = cont[index]
+	default:
+		return nil, fmt.Errorf("Unexpected value type: %v", reflect.ValueOf(c.Value))
+	}
+	node, ok := elem.(*Node)
+	if !ok {
+		return nil, fmt.Errorf("Unexpected element type: %v", reflect.ValueOf(elem))
+	}
+	return node.Value, nil
+}
+
+func (c Node) GetKey(key string) (interface{}, bool, error) {
+	om, ok := c.Value.(*OrderedMap)
+	if !ok {
+		return nil, false, fmt.Errorf("Unexpected value type: %v", reflect.ValueOf(c.Value))
+	}
+	elem, ok := om.Map[key]
+	if !ok {
+		return nil, false, nil
+	}
+	node, ok := elem.(*Node)
+	if !ok {
+		return nil, false, fmt.Errorf("Unexpected element type: %v", reflect.ValueOf(elem))
+	}
+	return node.Value, true, nil
+}
+
+func (c Node) SetIndex(index int, value interface{}) error {
+	var elem interface{}
+
+	switch cont := c.Value.(type) {
+	case *OrderedMap:
+		elem = cont.AtIndex(index)
+	case []interface{}:
+		elem = cont[index]
+	default:
+		return fmt.Errorf("Unexpected value type: %v", reflect.ValueOf(c.Value))
+	}
+	node, ok := elem.(*Node)
+	if ok {
+		node.Value = value
+	} else {
+		switch cont := c.Value.(type) {
+		case *OrderedMap:
+			cont.Map[cont.Keys[index]] = &Node{Value: value}
+		case []interface{}:
+			cont[index] = &Node{Value: value}
+		}
+	}
+
+	return nil
+}
+
+func (c Node) SetKey(key string, value interface{}) error {
+	om, ok := c.Value.(*OrderedMap)
+	if !ok {
+		return fmt.Errorf("Unexpected value type: %v", reflect.ValueOf(c.Value))
+	}
+	elem, ok := om.Map[key]
+	if ok {
+		var node *Node
+		node, ok = elem.(*Node)
+		if ok {
+			node.Value = value
+		}
+	}
+	if !ok {
+		om.Map[key] = &Node{Value: value}
+	}
+	return nil
 }
 
 func (c Node) MarshalJSON() ([]byte, error) {
--- node_test.go
+++ node_test.go
@@ -42,6 +42,30 @@ a: 2
 	var node *Node
 	Unmarshal([]byte(txt), &node)
 
+	if node.Len() != 2 {
+		t.Errorf("Unexpected map length: %v", node.Len())
+	}
+
+	aVal, ok, err := node.GetKey("a")
+	if err != nil {
+		t.Error(err)
+	}
+	// The value will be a float64 even though it was written without decimals.
+	if aVal != 2.0 {
+		t.Errorf("Unexpected value for key 'a': %v", aVal)
+	} else if !ok {
+		t.Errorf("node.GetKey('a') returned false")
+	}
+
+	bVal, err := node.GetIndex(0)
+	if err != nil {
+		t.Error(err)
+	}
+	// The value will be a float64 even though it was written without decimals.
+	if bVal != 1.0 {
+		t.Errorf("Unexpected value for key 'b': %v", bVal)
+	}
+
 	verifyNodeContent(t, node, txt)
 
 	opt := DefaultOptions()
@@ -60,11 +84,130 @@ a: 2
 
 	compareStrings(t, bOut, `{"b":1,"a":2}`)
 
-	node.Value.(*OrderedMap).Map["b"].(*Node).Value = 3
+	intLen := node.Value.(*OrderedMap).Map["b"].(*Node).Len()
+	if intLen != 0 {
+		t.Errorf("Unexpected int length: %v", intLen)
+	}
+
+	node.SetIndex(0, 3)
 
 	verifyNodeContent(t, node, `# comment before
 b: 3  # comment after
 // Comment B4
 a: 2
 /* Last comment */`)
+
+	node.SetKey("b", "abcdef")
+
+	verifyNodeContent(t, node, `# comment before
+b: "abcdef"  # comment after
+// Comment B4
+a: 2
+/* Last comment */`)
+
+	strLen := node.Value.(*OrderedMap).Map["b"].(*Node).Len()
+	if strLen != 6 {
+		t.Errorf("Unexpected string length: %v", strLen)
+	}
+
+	node.Value.(*OrderedMap).Map["b"] = "xyz"
+
+	verifyNodeContent(t, node, `b: xyz
+// Comment B4
+a: 2
+/* Last comment */`)
+}
+
+func TestNode3(t *testing.T) {
+	txt := `# comment before
+[
+# after [
+  1  # comment after
+  // Comment B4
+  2
+    # COmment After
+]
+/* Last comment */`
+
+	var node *Node
+	Unmarshal([]byte(txt), &node)
+
+	if node.Len() != 2 {
+		t.Errorf("Unexpected slice length: %v", node.Len())
+	}
+
+	firstVal, err := node.GetIndex(0)
+	if err != nil {
+		t.Error(err)
+	}
+	// The value will be a float64 even though it was written without decimals.
+	if firstVal != 1.0 {
+		t.Errorf("Unexpected value for index 0: %v", firstVal)
+	}
+
+	verifyNodeContent(t, node, txt)
+
+	opt := DefaultOptions()
+	opt.Comments = false
+	bOut, err := MarshalWithOptions(node, opt)
+	if err != nil {
+		t.Error(err)
+	}
+
+	compareStrings(t, bOut, `[
+  1
+  2
+]`)
+
+	bOut, err = json.Marshal(node)
+
+	compareStrings(t, bOut, `[1,2]`)
+
+	intLen := node.Value.([]interface{})[1].(*Node).Len()
+	if intLen != 0 {
+		t.Errorf("Unexpected int length: %v", intLen)
+	}
+
+	node.SetIndex(1, "abcdef")
+
+	verifyNodeContent(t, node, `# comment before
+[
+# after [
+  1  # comment after
+  // Comment B4
+  abcdef
+    # COmment After
+]
+/* Last comment */`)
+
+	strLen := node.Value.([]interface{})[1].(*Node).Len()
+	if strLen != 6 {
+		t.Errorf("Unexpected string length: %v", strLen)
+	}
+
+	node.Value.([]interface{})[0] = "xyz"
+
+	verifyNodeContent(t, node, `# comment before
+[
+  xyz
+  // Comment B4
+  abcdef
+    # COmment After
+]
+/* Last comment */`)
+}
+
+func TestNode4(t *testing.T) {
+	txt := `# comment before
+b: /* key comment */ {
+  sub1: 1  # comment after
+} # cm after obj
+// Comment B4
+a: 2
+/* Last comment */`
+
+	var node *Node
+	Unmarshal([]byte(txt), &node)
+
+	verifyNodeContent(t, node, txt)
 }

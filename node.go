422254d222f0c9517c67a6a414b34982bdb02e37
trobro <git@trobro.se>
2022-12-07 12:04:15 +0100

    fix node error messages

--- node.go
+++ node.go
@@ -44,11 +44,11 @@ func (c *Node) AtIndex(index int) (interface{}, error) {
 	case []interface{}:
 		elem = cont[index]
 	default:
-		return nil, fmt.Errorf("Unexpected value type: %#v", reflect.ValueOf(c.Value))
+		return nil, fmt.Errorf("Unexpected value type: %v", reflect.TypeOf(c.Value))
 	}
 	node, ok := elem.(*Node)
 	if !ok {
-		return nil, fmt.Errorf("Unexpected element type: %#v", reflect.ValueOf(elem))
+		return nil, fmt.Errorf("Unexpected element type: %v", reflect.TypeOf(elem))
 	}
 	return node.Value, nil
 }
@@ -59,7 +59,7 @@ func (c *Node) AtKey(key string) (interface{}, bool, error) {
 	}
 	om, ok := c.Value.(*OrderedMap)
 	if !ok {
-		return nil, false, fmt.Errorf("Unexpected value type: %#v", reflect.ValueOf(c.Value))
+		return nil, false, fmt.Errorf("Unexpected value type: %v", reflect.TypeOf(c.Value))
 	}
 	elem, ok := om.Map[key]
 	if !ok {
@@ -67,7 +67,7 @@ func (c *Node) AtKey(key string) (interface{}, bool, error) {
 	}
 	node, ok := elem.(*Node)
 	if !ok {
-		return nil, false, fmt.Errorf("Unexpected element type: %#v", reflect.ValueOf(elem))
+		return nil, false, fmt.Errorf("Unexpected element type: %v", reflect.TypeOf(elem))
 	}
 	return node.Value, true, nil
 }
@@ -101,7 +101,7 @@ func (c *Node) SetIndex(index int, value interface{}) error {
 	case []interface{}:
 		elem = cont[index]
 	default:
-		return fmt.Errorf("Unexpected value type: %#v", reflect.ValueOf(c.Value))
+		return fmt.Errorf("Unexpected value type: %v", reflect.TypeOf(c.Value))
 	}
 	node, ok := elem.(*Node)
 	if ok {
@@ -129,7 +129,7 @@ func (c *Node) SetKey(key string, value interface{}) error {
 		var ok bool
 		om, ok = c.Value.(*OrderedMap)
 		if !ok {
-			return fmt.Errorf("Unexpected value type: %#v", reflect.ValueOf(c.Value))
+			return fmt.Errorf("Unexpected value type: %v", reflect.TypeOf(c.Value))
 		}
 	}
 	elem, ok := om.Map[key]
